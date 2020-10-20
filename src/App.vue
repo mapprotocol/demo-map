790e555be72e8c63f2622fb4461587849749138e
xiyuhan994 <15194526412@163.com>
2020-10-20 11:52:48 +0800

    app-bg

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@ export default {
   height: 100%;
   overflow-x: hidden;
   color: #fff;
-  background-color: #050303;
+  background-color: #172029;
   /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
   /*-webkit-font-smoothing: antialiased;*/
   /*-moz-osx-font-smoothing: grayscale;*/
